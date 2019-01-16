d0962675ba89f00cf60945c290305fef1ecf0d20
mingmingdiii <mingmingdiii@gmail.com>
2019-01-16 15:59:21 +0800

    Delete m.py

--- main_code/m.py
+++
@@ -1,6 +0,0 @@
-import scipy.misc as misc
-from PIL import Image
-import numpy as np
-a = Image.open('/mnt/sda1/don/documents/project_paper/video_seg/data/youtube_vos/valid/Annotations/0a49f5265b/00000.png')
-a = np.array(a)
-print(a)
